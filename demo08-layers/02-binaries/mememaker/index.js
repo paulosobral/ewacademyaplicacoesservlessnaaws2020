edbcb4f5e7b954b9e205f5fd60956d53f1ceba9d
Paulo Sobral <paulo.sobral@outlook.com.br>
2025-02-05 21:58:54 -0300

    feat: class 03 module 06

---
+++ demo08-layers/02-binaries/mememaker/index.js
@@ -0,0 +1,14 @@
+module.exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v3.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+// TODO: 04:37 https://play.ewacademy.com.br/area/produto/item/2981394
