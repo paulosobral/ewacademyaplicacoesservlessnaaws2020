4b0b5f355f463f2899254ecea998c6d86fa51cbc
Paulo Sobral <paulo.sobral@outlook.com.br>
2025-02-09 19:26:55 -0300

    wip: class 03 module 06

--- demo08-layers/02-binaries/mememaker/index.js
+++ demo08-layers/02-binaries/mememaker/index.js
@@ -1,14 +1,52 @@
-module.exports.handler = async (event) => {
-  return {
+'use strict';
+const { randomUUID } = require('node:crypto')
+const axios = require('axios');
+const { promises: { writeFile, readFile, unlink}} = require('fs')
+const mememaker = require('@erickwendel/meme-maker')
+
+class Handler {
+  static generateImagePath() {
+    const isLocal = process.env.IS_OFFLINE
+    return `${isLocal ? "" : "/tmp/"}${randomUUID()}.png`
+  }
+  static async saveImageLocally(imageUrl, imagePath) {
+    const { data } = await axios.get(imageUrl, { responseType: 'arraybuffer' })
+    const buffer = Buffer.from(data, 'base64')
+    return writeFile(imagePath, buffer)
+  }
+  static async generateImageBbase64(imagePath) {
+    return readFile(imagePath, { encoding: 'base64' })
+  }
+}
+
+module.exports.mememaker = async (event) => {
+  const options = event.queryStringParameters
+  const imagePath = Handler.generateImagePath()
+  console.log('downloading image...')
+  await Handler.saveImageLocally(options.image, imagePath)
+  const imageOutputPath = Handler.generateImagePath()
+  console.log('generating meme image...')
+  await mememaker ({
+    image: imagePath,
+    outfile: imageOutputPath,
+    topText: options.topText,
+    bottomText: options.bottomText
+  })
+  console.log('generating base64')
+  const imageBuffer = await Handler.generateImageBbase64(imageOutputPath)
+  console.log('cleaning up!')
+  await Promise.all([
+    unlink(imagePath),
+    unlink(imageOutputPath)
+  ])
+  const response = {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    headers: {
+      'Content-Type': 'text/html'
+    },
+    body: `<img src="data:image/png;base64,${imageBuffer}" />`
+  }
+
+  return response
 };
-// TODO: 04:37 https://play.ewacademy.com.br/area/produto/item/2981394
+// TODO: 19:58 https://play.ewacademy.com.br/area/produto/item/2981394
