acd7b6090c475836158f7b1dc3d6ca8521e0f30f
Paulo Sobral <paulo.sobral@outlook.com.br>
2025-02-12 07:58:24 -0300

    feat: class 03 module 06

---
+++ demo08-layers/02-binaries/mememaker/run.sh
@@ -0,0 +1,3 @@
+http://localhost:3000/mememaker?image=https://wallpapers.com/images/hd/cartoon-character-homer-simpson-f0ggak0j97oog2t3.jpg&topText=HOJE É DIA DE&bottomText=DEPLOY
+
+http://localhost:3000/mememaker?image=https://ogimg.infoglobo.com.br/in/5499907-803-bba/FT1086A/760/cult_homersimpson.jpg&topText=HOJE É DIA DE&bottomText=MALDADE
