27d62d540b4cebda50baab02953387e6813a6212
Paulo Sobral <paulo.sobral@outlook.com.br>
2025-03-19 08:37:59 -0300

    feat: module 07 class 02

--- demo09-tooling/tooling-s3-sqs/index.js
+++ demo09-tooling/tooling-s3-sqs/index.js
@@ -1,13 +1,27 @@
+'use strict'
+const AWS = require('aws-sdk')
+
+const host = process.env.LOCALSTACK_HOST || 'localhost'
+const s3port = process.env.S3_PORT || '4572'
+const s3config = {
+  s3ForcePathStyle: true,
+  endpoint: new AWS.Endpoint(
+    `http://${host}:${s3port}`
+  )
+}
+const s3 = new AWS.S3(s3config)
+
 module.exports.handler = async (event) => {
+  const allBuckets = await s3.listBuckets().promise()
+  console.log('found', allBuckets)
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello!',
+        allBuckets,
       },
       null,
       2
     ),
   };
 };
-// TODO: 11:09 https://play.ewacademy.com.br/area/produto/item/2979171
